06ab26a07a1ca57eea026a9d0bdcf59d82b4587b
navami21 <159868453+navami21@users.noreply.github.com>
2024-11-25 15:47:34 +0530

    index.jsx

--- index.jsx
+++ index.jsx
@@ -8,7 +8,7 @@ import CityInput from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <><React.StrictMode>
-{/*     <App /> */}
+    {/* <App /> */}
   </React.StrictMode><CityInput /></>
 );
 
